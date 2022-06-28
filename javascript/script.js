b546d12d30cc0978ddf8f735f5492441cd85453e
ngocthinhhuynh02 <108309307+ngocthinhhuynh02@users.noreply.github.com>
2022-06-28 07:38:35 +0700

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+    chuyenTrang();
+    muaHang();
+});
+function chuyenTrang() {
+    $(".menu .trangchu").click(function () {
+        $(".menu li a").attr("class", "");
+        $(".menu .trangchu a").attr("class", "colorbaclk");
+        $("#main main").hide(100);
+        $("#main .container").stop().toggle(100);
+    });
+    $(".menu .vechungtoi").click(function () {
+        $(".menu li a").attr("class", "");
+        $(".menu .vechungtoi a").attr("class", "colorbaclk");
+        $("#main main").hide(100);
+        $("#main .veChungToi").stop().toggle(100);
+    });
+    $(".menu .khuyenmai").click(function () {
+        $(".menu li a").attr("class", "");
+        $(".menu .khuyenmai a").attr("class", "colorbaclk");
+        $("#main main").hide(100);
+        $("#main .khuyenMai").stop().toggle(100);
+    });
+    $(".menu .dichvu").click(function () {
+        $(".menu li a").attr("class", "");
+        $(".menu .dichvu a").attr("class", "colorbaclk");
+        $("#main main").hide(100);
+        $("#main .dichVu").stop().toggle(100);
+    });
+    $(".menu .tintuc").click(function () {
+        $(".menu li a").attr("class", "");
+        $(".menu .tintuc a").attr("class", "colorbaclk");
+        $("#main main").hide(100);
+        $("#main .tinTuc").stop().toggle(100);
+    });
+    $(".menu .lienhe").click(function () {
+        $(".menu li a").attr("class", "");
+        $(".menu .lienhe a").attr("class", "colorbaclk");
+        $("#main main").hide(100);
+        $("#main .lienHe").stop().toggle(100);
+    });
+}
+function muaHang() {
+    $(".buy").click(function () {
+        window.location.href = 'muahang.html';
+    });
+}
